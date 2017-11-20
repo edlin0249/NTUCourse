c6218a625bc01cb1d03bd1c89fad51808d4f0631
Jay <jay@Jays-MacBook-Pro.local>
2017-11-20 13:25:19 +0800

    CA_hw4

---
+++ 2017 Fall/CSIE 3340 - Computer Architecture/homeworks/hw4/hw4_b03902129_v0/src/Sign_Extend.v
@@ -0,0 +1,17 @@
+module Sign_Extend(
+	data_i,	// inst[15:0]
+	data_o	// MUX_ALUSrc.data2_i
+);
+
+input	[15:0]	data_i;
+output	[31:0]	data_o;
+
+reg	[31:0]	tmp;
+assign	data_o = tmp;
+
+always@(*) begin
+	tmp[15:0] = data_i[15:0];
+	tmp[31:16] = data_i[15];
+end
+
+endmodule
